d13d55ecb9457a761bdd260f31b73e01a22d3755
Yaroslav Pyurko <yarickpy@gmail.com>
2026-08-03 21:07:59 +0300

    feat: Merge Sorted Array tests

---
+++ tests/LeetCode.Tests/LC0088_MergeSortedArrayTests.cs
@@ -0,0 +1,34 @@
+﻿namespace LeetCode.Tests
+{
+    public class LC0088_MergeSortedArrayTests
+    {
+        [Theory]
+        [InlineData(
+            new int[] { 1, 2, 3, 0, 0, 0 }, 
+            3,
+            new int[] { 2, 5, 6 }, 
+            3,
+            new int[] { 1, 2, 2, 3, 5, 6 })]
+        [InlineData(
+            new int[] { 1 },
+            1,
+            new int[] { },
+            0,
+            new int[] { 1 })]
+        [InlineData(
+            new int[] { 0 },
+            0,
+            new int[] { 1 },
+            1,
+            new int[] { 1 })]
+        public void Merge_ReturnsExpectedResult(int[] nums1, int m, int[] nums2, int n, int[] expected)
+        {
+            var solution = new LC0088_MergeSortedArray();
+
+            solution.Merge(nums1, m, nums2, n);
+            var actual = nums1;
+
+            Assert.Equal(expected, actual);
+        }
+    }
+}
