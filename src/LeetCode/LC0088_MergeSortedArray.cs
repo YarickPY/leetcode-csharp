5d16e6393dfe8cd5692d115a896c90ef92b7eec2
Yaroslav Pyurko <yarickpy@gmail.com>
2026-08-04 15:50:20 +0300

    feat: Merge Sorted Array stub

---
+++ src/LeetCode/LC0088_MergeSortedArray.cs
@@ -0,0 +1,15 @@
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace LeetCode
+{
+    public class LC0088_MergeSortedArray
+    {
+        public void Merge(int[] nums1, int m, int[] nums2, int n)
+        {
+            var oldLength = m;
+            var newLength = m + n;
+            Array.Resize(ref nums1, newLength);
+            Array.Copy(nums2, 0, nums1, oldLength, nums2.Length);
+        }
+    }
+}
