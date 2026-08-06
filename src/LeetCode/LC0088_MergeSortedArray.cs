7862a484bcea7562198b4b535ef28940dac0e4ce
Yaroslav Pyurko <yarickpy@gmail.com>
2026-08-06 15:35:17 +0300

    feat: add maximum acceleration via Array.Copy

--- src/LeetCode/LC0088_MergeSortedArray.cs
+++ src/LeetCode/LC0088_MergeSortedArray.cs
@@ -4,6 +4,31 @@
     {
         public void Merge(int[] nums1, int m, int[] nums2, int n)
         {
+            // Required data validation
+            if (n == 0)
+            {
+                return;
+            }
+            if (m == 0)
+            {
+                Array.Copy(nums2, 0, nums1, 0, n);
+                return;
+            }
+
+            // Probable and fastest path 1, when merging data chunks
+            if (nums1[m - 1] <= nums2[0])
+            {
+                Array.Copy(nums2, 0, nums1, m, n);
+                return;
+            }
+            if (nums2[n - 1] <= nums1[0])
+            {
+                Array.Copy(nums1, 0, nums1, n, m);
+                Array.Copy(nums2, 0, nums1, 0, n);
+                return;
+            }
+
+            // General case, merge from the back
             var i = m - 1;
             var j = n - 1;
             var k = m + n - 1;
@@ -23,12 +48,9 @@
                 k--;
             }
 
-            // nums2 still has elements, copy them
-            while (j >= 0)
+            if (j >= 0)
             {
-                nums1[k] = nums2[j];
-                j--;
-                k--;
+                Array.Copy(nums2, 0, nums1, 0, j + 1);
             }
         }
     }
