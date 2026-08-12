6fdfc22710434bd5ef5d477f9a391390d0a6f661
Yaroslav Pyurko <yarickpy@gmail.com>
2026-08-12 22:31:41 +0300

    feat: add Number Of Islands

---
+++ src/LeetCode/LC0200_NumberOfIslands.cs
@@ -0,0 +1,45 @@
+﻿namespace LeetCode
+{
+    public class LC0200_NumberOfIslands
+    {
+        public int NumIslands(char[][] grid)
+        {
+            if (grid == null || grid.Length == 0) return 0;
+            int numIslands = 0;
+            for (int i = 0; i < grid.Length; i++)
+            {
+                for (int j = 0; j < grid[i].Length; j++)
+                {
+                    if (grid[i][j] == '1')
+                    {
+                        numIslands++;
+                        Dfs(grid, i, j);
+                    }
+                }
+            }
+            return numIslands;
+        }
+
+        /// <summary>
+        /// Depth-first search to mark all connected land cells ('1') as visited ('0').
+        /// </summary>
+        private void Dfs(char[][] grid, int i, int j)
+        {
+            if (i < 0               ||
+                i >= grid.Length    ||
+                j < 0               ||
+                j >= grid[i].Length ||
+                grid[i][j] == '0')
+            {
+                return;
+            }
+
+            // Set the current cell to mark it as visited
+            grid[i][j] = '0';
+            Dfs(grid, i + 1, j);
+            Dfs(grid, i - 1, j);
+            Dfs(grid, i, j + 1);
+            Dfs(grid, i, j - 1);
+        }
+    }
+}
