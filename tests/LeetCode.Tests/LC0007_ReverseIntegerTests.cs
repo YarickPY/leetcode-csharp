29fc497d14b25b541c1972789511ced166e85e7d
Yaroslav Pyurko <yarickpy@gmail.com>
2026-08-24 20:38:08 +0300

    fix: miscalculation in comments

--- tests/LeetCode.Tests/LC0007_ReverseIntegerTests.cs
+++ tests/LeetCode.Tests/LC0007_ReverseIntegerTests.cs
@@ -6,7 +6,7 @@
         [InlineData(123, 321)]
         [InlineData(-123, -321)]
         [InlineData(120, 21)]
-        [InlineData(2_147_483_647, 0)] // 2^32-1, overflow case
+        [InlineData(2_147_483_647, 0)] // 2^31-1, overflow case
         [InlineData(111_222_333, 333_222_111)]
         public void Reverse_ReturnsExpectedResult(int input, int expected)
         {
