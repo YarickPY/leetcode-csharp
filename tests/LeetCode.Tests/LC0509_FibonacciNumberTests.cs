751abcbe35476d9d234563540e621dd7b9683e73
Yaroslav Pyurko <yarickpy@gmail.com>
2026-09-02 10:14:12 +0300

    feat: new Fibonacci Number tests

--- tests/LeetCode.Tests/LC0509_FibonacciNumberTests.cs
+++ tests/LeetCode.Tests/LC0509_FibonacciNumberTests.cs
@@ -2,14 +2,22 @@
 {
     public class LC0509_FibonacciNumberTests
     {
-        [Fact]
-        public void Case1()
+        [Theory]
+        [InlineData(0, 0)]
+        [InlineData(1, 1)]
+        [InlineData(2, 1)]
+        [InlineData(3, 2)]
+        [InlineData(4, 3)]
+        [InlineData(10, 55)]
+        [InlineData(30, 832_040)]        // upper bound of the LeetCode constraints
+        [InlineData(46, 1_836_311_903)]  // largest Fibonacci number that fits in int
+        public void Fib_ReturnsExpectedResult(int n, int expected)
         {
             var solution = new LC0509_FibonacciNumber();
 
-            var actual = solution.Fib(30);
+            var actual = solution.Fib(n);
 
-            Assert.Equal(832_040, actual);
+            Assert.Equal(expected, actual);
         }
     }
 }
